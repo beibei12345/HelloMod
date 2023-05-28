6f133e1a4be8aea4c364e7533291896e6c076e02
beibeixyzxyz <1539730040@qq.com>
2023-05-28 18:29:53 +0800

    init

---
+++ src/main/java/zhang/hello_mod/Main.java
@@ -0,0 +1,22 @@
+package zhang.hello_mod;
+
+import net.minecraft.Util;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.UUID;
+
+@Mod("hello_mod")
+@Mod.EventBusSubscriber
+public class Main {
+    @SubscribeEvent
+    public static void playerJoinWorld(PlayerEvent.PlayerLoggedInEvent event){
+        Player player = event.getPlayer();
+        Level level = player.level;
+        player.sendMessage(new TextComponent("hello " + player.getDisplayName().getString() + ". from " + (level.isClientSide() ? "CLIENT" : "SERVER")), Util.NIL_UUID);
+    }
+}
